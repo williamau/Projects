7df290bbbcf59236e63dae766c0e579dc2cd3f83
williamau <williamyang7706@hotmail.com>
2015-05-01 09:39:15 +1000

    Add another line

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace MyFirstConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Another Line");
             Console.ReadLine();
         }
     }
